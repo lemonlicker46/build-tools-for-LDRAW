19ba5dd29cc034c7c61b9cd3f28432eb6fb936c6
lemonlicker46 <lemonlicker46@gmail.com>
2026-03-31 18:11:09 +0100

    Add files via upload

--- builder.bat
+++ builder.bat
@@ -4,7 +4,7 @@ set "localecode=%localecode: =%"
 chcp 65001 >nul
 setlocal enabledelayedexpansion
 
-:: Assess there is at least one argument
+:: Check for input
 if "%~1"=="" (
     echo Usage: %~nx0 "filename.ext"
     if defined localecode chcp %localecode% >nul 2>&1
@@ -12,7 +12,7 @@ if "%~1"=="" (
     exit /b 1
 )
 
-:: Rebuild full path from all arguments (supports unquoted spaces)
+:: Rebuild full path
 set "input=%~1"
 shift
 :collectArgs
@@ -22,37 +22,14 @@ if not "%~1"=="" (
     goto collectArgs
 )
 
-:: Create temporary VBScript helper in %temp%
-set "tmpvbs=%temp%\validate_filename_unicode.vbs"
-> "%tmpvbs%" echo s = WScript.Arguments(0)
->> "%tmpvbs%" echo For i = 1 To Len(s)
->> "%tmpvbs%" echo   c = AscW(Mid(s, i, 1))
->> "%tmpvbs%" echo   If c ^< 32 Or c ^> 127 Then
->> "%tmpvbs%" echo     WScript.Quit 1
->> "%tmpvbs%" echo   End If
->> "%tmpvbs%" echo Next
->> "%tmpvbs%" echo WScript.Quit 0
-
-cscript //nologo "%tmpvbs%" "%input%"
-set "vbsExit=%errorlevel%"
-if "%vbsExit%" NEQ "0" (
-    if exist "%tmpvbs%" del "%tmpvbs%" >nul 2>&1
-    if defined localecode chcp %localecode% >nul 2>&1
-    echo Error: filename contains forbidden non-ASCII or invisible character.
-    endlocal
-    exit /b 1
-)
-
-
-:: Normalize path and extract name/ext
+:: Normalize path
 for %%F in ("%input%") do (
     set "fullpath=%%~fF"
     set "name=%%~nF"
     set "ext=%%~xF"
 )
 
-::DEBUG:: echo [DBG-CHK] fullpath=%fullpath% name=%name% ext=%ext%
-
+:: File checks
 if not exist "%fullpath%" (
     echo Error: File "%fullpath%" not found.
     if defined localecode chcp %localecode% >nul 2>&1
@@ -60,7 +37,6 @@ if not exist "%fullpath%" (
     exit /b 1
 )
 
-::DEBUG::  echo [DBG-CHK] file exists
 if exist "%fullpath%\*" (
     echo Error: "%fullpath%" is a directory.
     if defined localecode chcp %localecode% >nul 2>&1
@@ -68,7 +44,7 @@ if exist "%fullpath%\*" (
     exit /b 1
 )
 
-::DEBUG:: echo [DBG-CHK] ext=%ext%
+:: Extension check
 if /i "%ext%"==".ldr" goto ext_ok
 if /i "%ext%"==".dat" goto ext_ok
 if /i "%ext%"==".mpd" goto ext_ok
@@ -80,50 +56,76 @@ exit /b 1
 
 :ext_ok
 
-::DEBUG:: echo [DBG-CHK] in ext_ok
-
-:: Extract dimension token = last token in base name
+:: Extract last token (dimensions)
 set "last="
 for %%A in (%name%) do set "last=%%A"
-::DEBUG:: echo [DBG-CHK] after last assignment last=!last!
 if "%last%"=="" set "last=%name%"
-::DEBUG:: echo [DBG-CHK] after fallback last=!last!
 
-cscript //nologo "%tmpvbs%" "%last%"
-::DEBUG:: echo [DBG-CHK] exit=%errorlevel%
-set "vbsLastExit=%errorlevel%"
-::DEBUG:: echo [DBG-CHK] vbsLastExit=%vbsLastExit%
+:: Create temp VBScript
+set "tmpvbs=%temp%\dim_to_ldu.vbs"
+
+> "%tmpvbs%" echo dim input : input = WScript.Arguments(0)
+>> "%tmpvbs%" echo input = Replace(input, "X", "x")
+>> "%tmpvbs%" echo parts = Split(input, "x")
+>> "%tmpvbs%" echo If UBound(parts) ^< 1 Then
+>> "%tmpvbs%" echo   WScript.Quit 1
+>> "%tmpvbs%" echo End If
+
+>> "%tmpvbs%" echo Function ToLDU(val)
+>> "%tmpvbs%" echo   val = Trim(val)
+>> "%tmpvbs%" echo   If val = "" Then ToLDU = "_" : Exit Function
+>> "%tmpvbs%" echo   unit = UCase(Right(val,1))
+>> "%tmpvbs%" echo   num = val
+>> "%tmpvbs%" echo   If unit = "L" Or unit = "P" Or unit = "S" Or unit = "B" Then
+>> "%tmpvbs%" echo     num = Left(val, Len(val)-1)
+>> "%tmpvbs%" echo   Else
+>> "%tmpvbs%" echo     unit = "L"
+>> "%tmpvbs%" echo   End If
+>> "%tmpvbs%" echo   If Not IsNumeric(num) Then ToLDU = "?" : Exit Function
+>> "%tmpvbs%" echo   num = CDbl(num)
+>> "%tmpvbs%" echo   Select Case unit
+>> "%tmpvbs%" echo     Case "L": factor = 20
+>> "%tmpvbs%" echo     Case "P": factor = 8
+>> "%tmpvbs%" echo     Case "S": factor = 16
+>> "%tmpvbs%" echo     Case "B": factor = 24
+>> "%tmpvbs%" echo   End Select
+>> "%tmpvbs%" echo   ToLDU = num * factor
+>> "%tmpvbs%" echo End Function
+
+>> "%tmpvbs%" echo d1 = ToLDU(parts(0))
+>> "%tmpvbs%" echo d2 = ToLDU(parts(1))
+>> "%tmpvbs%" echo If UBound(parts) ^>= 2 Then
+>> "%tmpvbs%" echo   d3 = ToLDU(parts(2))
+>> "%tmpvbs%" echo Else
+>> "%tmpvbs%" echo   d3 = "_"
+>> "%tmpvbs%" echo End If
+
+>> "%tmpvbs%" echo Function fmt(v)
+>> "%tmpvbs%" echo   If v = "_" Then
+>> "%tmpvbs%" echo     fmt = "_LDU"
+>> "%tmpvbs%" echo   ElseIf v = "?" Then
+>> "%tmpvbs%" echo     fmt = "?LDU"
+>> "%tmpvbs%" echo   Else
+>> "%tmpvbs%" echo     fmt = v ^& "LDU"
+>> "%tmpvbs%" echo   End If
+>> "%tmpvbs%" echo End Function
+
+>> "%tmpvbs%" echo WScript.Echo fmt(d1) ^& "x" ^& fmt(d2) ^& "x" ^& fmt(d3)
+>> "%tmpvbs%" echo WScript.Quit 0
+
+:: Run VBScript
+for /f "delims=" %%D in ('cscript //nologo "%tmpvbs%" "!last!"') do set "ldu=%%D"
+
 if exist "%tmpvbs%" del "%tmpvbs%" >nul 2>&1
-::DEBUG:: echo [DBG-CHK] after tmpvbs cleanup
-::DEBUG:: echo [DBG-CHK] before vbs condition
-if "%vbsLastExit%"=="0" goto vbs_check_done
-if defined localecode (chcp %localecode% >nul 2>&1)
-echo Error: file extension contains forbidden non-ASCII characters: "%last%"
-echo Error: File must be .ldr, .dat, or .mpd (found "%ext%").
-endlocal
-exit /b 1
 
-:vbs_check_done
+:: Align output
+set "left=Dimensions: !last!"
+set "pad=                                        "
+set "left=!left!!pad!"
+set "left=!left:~0,40!"
 
-::DEBUG:: echo [DBG-CHK] after vbs condition
+echo !left!!ldu!
 
-:: Strict NxM parse
-set "left="
-set "right="
-for /f "delims=xX" %%A in ("!last!") do set "left=%%A"
-for /f "tokens=2 delims=xX" %%A in ("!last!") do set "right=%%A"
-if "!left!"=="" goto invalid_format
-if "!right!"=="" goto invalid_format
-
-:: Success
-echo Dimensions: !last!
 if defined localecode chcp %localecode% >nul 2>&1
 endlocal
 exit /b 0
-
-:invalid_format
-echo Error: Dimensions must be in format NxM (found "!last!").
-if defined localecode chcp %localecode% >nul 2>&1
-endlocal
-exit /b 1
-
